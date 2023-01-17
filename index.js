282aaae061de10d75364a0cf358b96f2d793d07e
nnnigina <asslamova@mail.ru>
2023-01-17 11:05:33 +0300

    a1

---
+++ index.js
@@ -0,0 +1,25 @@
+// 1-е задание
+
+let name = 'John'
+let admin = ''
+console.log(admin = name);
+
+
+
+//2-е задание
+
+// баланс пользователя
+let balance = 25000
+
+// траты
+let car = 4480
+let food = 890
+
+// доходы
+let invoice = 5500
+let stock = 4200
+
+// итоги
+let totalBalance = balance - car - food + invoice + stock
+
+console.log(totalBalance);
